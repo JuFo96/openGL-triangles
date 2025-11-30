e4d081bd2cf6daefab1764f525dedf3dd91d7f08
Julius Foverskov <julius.foverskov1@gmail.com>
2025-11-30 12:37:18 +0100

    hello triangle working

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,22 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height); 
+void process_input(GLFWwindow* window);
+
+const char *vertexShaderSource = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
 
 int main() {
 
@@ -28,19 +44,106 @@ int main() {
         std::cout << "Failed to initialise GLAD" << std::endl;
         return -1;
     }
+
+    //Vertex data
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5, 0.0f,
+    0.0f, 0.5f, 0.0f,
+
+    };
+    // VBO
+    unsigned int VertexBufferObject;
+    glGenBuffers(1, &VertexBufferObject);
+    glBindBuffer(GL_ARRAY_BUFFER, VertexBufferObject);
+    // Sends data to GPU, GL_STATIC_DRAW implies data is set once and drawn many times
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     
+    //Error data for shader compilers
+    int success;
+    char infoLog[512];
+
+    // Vertex Shader ??
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Fragment Shader, determines colour
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Check for shader compile error
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Shader Program orchestrator??
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    // Link shaders to shaderProgram
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    unsigned int vertexArrayObject;
+    glGenVertexArrays(1, &vertexArrayObject);
+
+    glBindVertexArray(vertexArrayObject);
+    glBindBuffer(GL_ARRAY_BUFFER, VertexBufferObject);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+
+    // Render Loop
     while(!glfwWindowShouldClose(window)) {
-        glfwSwapBuffers(window);
+        // Input
+        process_input(window);
+        
+        // Rendering
+        glClearColor(0.2f, 0.3, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vertexArrayObject);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+
+
         glfwPollEvents();
+        glfwSwapBuffers(window);
     }
 
 
     return 0;
 }
 
+
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window) 
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
 }
